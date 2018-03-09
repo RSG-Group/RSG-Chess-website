9d1f9b65e0edab1b5794894e1fce889b972c5507
Radi Cholakov <radicho123@gmail.com>
2018-03-09 20:49:06 +0200

    Complete the grid design.

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -1,47 +1,184 @@
 import React from "react";
 import { Grid, Row, Col, Clearfix } from "react-bootstrap";
+import GitHubIcon from "../static/GitHubIcon";
 
-const dummySentences = [
-  'Lorem ipsum dolor sit amet, consectetuer adipiscing elit.',
-  'Donec hendrerit tempor tellus.',
-  'Donec pretium posuere tellus.',
-  'Proin quam nisl, tincidunt et, mattis eget, convallis nec, purus.',
-  'Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.',
-  'Nulla posuere.',
-  'Donec vitae dolor.',
-  'Nullam tristique diam non turpis.',
-  'Cras placerat accumsan nulla.',
-  'Nullam rutrum.',
-  'Nam vestibulum accumsan nisl.'
-];
-  
-  const gridInstance = () => (
-    <Grid id="home-grid">
-      <Row>
-        <Col sm={6} md={6} className="grid-col row-eq-height">
-          {dummySentences.slice(0, 6).join(' ')}
-        </Col>
-        <Col sm={6} md={6} className="grid-col row-eq-height">
-          {dummySentences.slice(0, 4).join(' ')}
-        </Col>
-      </Row>
-      <Row>
-        <Col sm={6} md={6} className="grid-col row-eq-height">
-          {dummySentences.slice(0, 6).join(' ')}
-        </Col>
-        <Col sm={6} md={6} className="grid-col row-eq-height">
-          {dummySentences.slice(0, 4).join(' ')}
-        </Col>
-      </Row>
-      <Row>
-        <Col sm={6} md={6} className="grid-col row-eq-height">
-          {dummySentences.slice(0, 6).join(' ')}
-        </Col>
-        <Col sm={6} md={6} className="grid-col row-eq-height">
-          {dummySentences.slice(0, 4).join(' ')}
-        </Col>
-      </Row>
-    </Grid>
-  );
+const gridInstance = () => (
+  <Grid id="home-grid">
+    <Row>
+      <Col md={3} sm={6} xs={12} className="grid-col">
+        <span>
+          <GitHubIcon
+            style={{
+              display: "block",
+              margin: "auto",
+              width: "50px",
+              height: "50px"
+            }}
+          />
+          <div class="feature-title">Test content</div>
+          <div class="feature-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</div>
+        </span>
+      </Col>
+      <Col md={3} sm={6} xs={12} className="grid-col">
+        <span>
+          <GitHubIcon
+            style={{
+              display: "block",
+              margin: "auto",
+              width: "50px",
+              height: "50px"
+            }}
+          />
+          <div class="feature-title">Test content</div>
+          <div class="feature-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</div>
+        </span>
+      </Col>
+      <Col md={3} sm={6} xs={12} className="grid-col">
+        <span>
+          <GitHubIcon
+            style={{
+              display: "block",
+              margin: "auto",
+              width: "50px",
+              height: "50px"
+            }}
+          />
+          <div class="feature-title">Test content</div>
+          <div class="feature-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</div>
+        </span>
+      </Col>
+      <Col md={3} sm={6} xs={12} className="grid-col">
+        <span>
+          <GitHubIcon
+            style={{
+              display: "block",
+              margin: "auto",
+              width: "50px",
+              height: "50px"
+            }}
+          />
+          <div class="feature-title">Test content</div>
+          <div class="feature-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</div>
+        </span>
+      </Col>
+    </Row>
+    <Row>
+      <Col md={3} sm={6} xs={12} className="grid-col">
+        <span>
+          <GitHubIcon
+            style={{
+              display: "block",
+              margin: "auto",
+              width: "50px",
+              height: "50px"
+            }}
+          />
+          <div class="feature-title">Test content</div>
+          <div class="feature-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</div>
+        </span>
+      </Col>
+      <Col md={3} sm={6} xs={12} className="grid-col">
+        <span>
+          <GitHubIcon
+            style={{
+              display: "block",
+              margin: "auto",
+              width: "50px",
+              height: "50px"
+            }}
+          />
+          <div class="feature-title">Test content</div>
+          <div class="feature-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</div>
+        </span>
+      </Col>
+      <Col md={3} sm={6} xs={12} className="grid-col">
+        <span>
+          <GitHubIcon
+            style={{
+              display: "block",
+              margin: "auto",
+              width: "50px",
+              height: "50px"
+            }}
+          />
+          <div class="feature-title">Test content</div>
+          <div class="feature-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</div>
+        </span>
+      </Col>
+      <Col md={3} sm={6} xs={12} className="grid-col">
+        <span>
+          <GitHubIcon
+            style={{
+              display: "block",
+              margin: "auto",
+              width: "50px",
+              height: "50px"
+            }}
+          />
+          <div class="feature-title">Test content</div>
+          <div class="feature-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</div>
+        </span>
+      </Col>
+    </Row>
+    <Row>
+      <Col md={3} sm={6} xs={12} className="grid-col">
+        <span>
+          <GitHubIcon
+            style={{
+              display: "block",
+              margin: "auto",
+              width: "50px",
+              height: "50px"
+            }}
+          />
+          <div class="feature-title">Test content</div>
+          <div class="feature-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</div>
+        </span>
+      </Col>
+      <Col md={3} sm={6} xs={12} className="grid-col">
+        <span>
+          <GitHubIcon
+            style={{
+              display: "block",
+              margin: "auto",
+              width: "50px",
+              height: "50px"
+            }}
+          />
+          <div class="feature-title">Test content</div>
+          <div class="feature-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</div>
+        </span>
+      </Col>
+      <Col md={3} sm={6} xs={12} className="grid-col">
+        <span>
+          <GitHubIcon
+            style={{
+              display: "block",
+              margin: "auto",
+              width: "50px",
+              height: "50px"
+            }}
+          />
+          <div class="feature-title">Test content</div>
+          <div class="feature-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</div>
+        </span>
+      </Col>
+      <Col md={3} sm={6} xs={12} className="grid-col">
+        <span>
+          <GitHubIcon
+            style={{
+              display: "block",
+              margin: "auto",
+              width: "50px",
+              height: "50px"
+            }}
+          />
+          <div class="feature-title">Test content</div>
+          <div class="feature-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</div>
+        </span>
+      </Col>
+    </Row>
+  </Grid>
+);
 
 export default gridInstance;
