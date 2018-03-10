c787c2a839330649c7cc7b4416030dd6b48b2075
Radi Cholakov <radicho123@gmail.com>
2018-03-10 21:06:28 +0200

    Rewrite the Slider and remove the duplications.

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -3,21 +3,15 @@ import { Carousel } from "react-bootstrap";
 
 const Slider = () => (
   <Carousel id="home-slider" controls={false} interval={2500}>
-    <Carousel.Item>
-      <img src="/pictures/slide1.png" alt="This promo graphic cannot be displayed."/>
-    </Carousel.Item>
-    <Carousel.Item>
-      <img src="/pictures/slide2.png" alt="This promo graphic cannot be displayed."/>
-    </Carousel.Item>
-    <Carousel.Item>
-      <img src="/pictures/slide3.png" alt="This promo graphic cannot be displayed."/>
-    </Carousel.Item>
-    <Carousel.Item>
-      <img src="/pictures/slide4.png" alt="This promo graphic cannot be displayed."/>
-    </Carousel.Item>
-    <Carousel.Item>
-      <img src="/pictures/slide5.png" alt="This promo graphic cannot be displayed."/>
-    </Carousel.Item>
+    {[1, 2, 3, 4, 5].map(ev => (
+      <Carousel.Item>
+        <img
+          key={ev}
+          src={`/pictures/slide${ev}.png`}
+          alt="This promo graphic cannot be displayed."
+        />
+      </Carousel.Item>
+    ))}
   </Carousel>
 );
 
